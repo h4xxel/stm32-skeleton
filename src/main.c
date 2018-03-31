09cc2f73f9a33f0b3ae2d2871991f20a6d90acaf
Axel Isaksson <isaksson.axel@telia.com>
2018-03-31 22:36:24 +0200

    setup 48 MHz clock

--- src/main.c
+++ src/main.c
@@ -12,7 +12,21 @@ void delay(volatile uint32_t i) {
 	for(; i > 0; i--);
 }
 
+void setup_clock() {
+	/* Set up internal 48 MHz as system and peripheral clock */
+	RCC.cr2.hsi48_on = true;
+	while(!RCC.cr2.hsi48_rdy);
+	
+	RCC.cfgr.sw = 0x3;
+	while(RCC.cfgr.sw != 0x3);
+	
+	RCC.cfgr.hpre = 0x0;
+	RCC.cfgr.ppre = 0x0;
+}
+
 int main() {
+	setup_clock();
+	
 	RCC.ahb_enr.gpio_b_en = true;
 	
 	GPIOB.mode.pin1 = STM32_GPIO_MODE_OUTPUT;
